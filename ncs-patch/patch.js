42acb0f18e4e288365eef739474ec5bfbbde6235
Tae Lim Kook <tkook11@gmail.com>
2014-12-26 15:12:40 -0500

    Fixed again

--- ncs-patch/patch.js
+++ ncs-patch/patch.js
@@ -3,9 +3,9 @@
 		"nmlgc/base_tsa",
 		"nmlgc/script_latin"
 	],
-	"id": ".",
+	"id": "ncs-patch",
 	"servers": [
-		"https://raw.githubusercontent.com/bluebear94/bb94-thcrap/master/./"
+		"https://raw.githubusercontent.com/bluebear94/bb94-thcrap/master/ncs-patch/"
 	],
 	"title": "BB94's THCrap Patch Server (Necarasso Cryssesa translations)"
 }
---
+++ repo.js
@@ -0,0 +1,11 @@
+{
+	"contact": "tkook11@gmail.com",
+	"id": "bb94",
+	"patches": {
+		"ncs-patch": "BB94's THCrap Patch Server (Necarasso Cryssesa translations)"
+	},
+	"servers": [
+		"https://raw.githubusercontent.com/bluebear94/bb94-thcrap/master/"
+	],
+	"title": "BB94's nice THCrap Repository"
+}
