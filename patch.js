b558dc1006a5b47939104ead81202fa222f1e505
Tae Lim Kook <tkook11@gmail.com>
2014-12-26 14:47:57 -0500

    updated repo

--- patch.js
+++ patch.js
@@ -1,6 +1,11 @@
 {
-  "dependencies": [
-    "nmlgc/base_tsa",
-    "nmlgc/script_latin"
-  ]
+	"dependencies": [
+		"nmlgc/base_tsa",
+		"nmlgc/script_latin"
+	],
+	"id": ".",
+	"servers": [
+		"https://raw.githubusercontent.com/bluebear94/bb94-thcrap/master/./"
+	],
+	"title": "BB94's THCrap Patch Server (Necarasso Cryssesa translations)"
 }
---
+++ repo.js
@@ -0,0 +1,11 @@
+{
+	"contact": "tkook11@gmail.com",
+	"id": "bb94-thcrap",
+	"patches": {
+		".": "BB94's THCrap Patch Server (Necarasso Cryssesa translations)"
+	},
+	"servers": [
+		"https://raw.githubusercontent.com/bluebear94/bb94-thcrap/master/"
+	],
+	"title": "BB94's THCrap Server (Necarasso Cryssesa patch)"
+}
