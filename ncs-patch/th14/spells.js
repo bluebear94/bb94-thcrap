6b5ccf991bb60ebed40f0041b7ced9971b191815
Tae Lim Kook <tkook11@gmail.com>
2014-12-26 15:35:34 -0500

    Added some spellcard names

---
+++ ncs-patch/th14/spells.js
@@ -0,0 +1,9 @@
+{
+  "0": "Arcesa asage «Merva Arcynercedjon»",
+  "2": "Nyrsesa asage «Gartenlesa Mortata»",
+  "6": "Ecresa asage «Ecresa Creten»",
+  "8": "Ecresa asage «Yšymela Ecra eas Ardsso Crečyn»",
+  "9": "Ecresa asage «Yšymela Ecra eas Mervo Ardsso Crečyn»",
+  "10": "Voratesa asage «Vore Lemes»"
+  "14": "Čelesa asage «Enpros-Menacesa Cissenata»"
+}
